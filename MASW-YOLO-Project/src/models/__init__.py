876a5c29cad3aec729528d2033a2a8a9488ebf72
alizeinaldini80-dot <alizeinaldini80@gmail.com>
2026-07-04 01:19:30 +0330

    Delete MASW-YOLO-Project directory

--- MASW-YOLO-Project/src/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# MASW-YOLO Models
-# Base model definitions and configurations.
-
-from .modules.msca import MSCA
-from .modules.afpn import AFPN
-from .custom_model import MASWDetectionModel
-
-__all__ = ["MSCA", "AFPN", "MASWDetectionModel"]
